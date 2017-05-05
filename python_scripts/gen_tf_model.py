f08a07d00c083c9b91cd9604cc77694dd431c332
Tyler Zeller <tylermzeller@gmail.com>
2017-05-05 15:37:01 -0400

    Update gen_tf_model.py

--- python_scripts/gen_tf_model.py
+++ python_scripts/gen_tf_model.py
@@ -8,7 +8,7 @@ with tf.Session() as sess:
     W = tf.Variable(tf.zeros([784, 10]), name="weights")
     b = tf.Variable(tf.zeros([10]))
 
-    y_out = tf.nn.softmax(tf.matmul(x, W) + b, name="y_out")
+    y_out = tf.matmul(x, W) + b
 
     #cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y_out), reduction_indices=[1]))
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_out))
